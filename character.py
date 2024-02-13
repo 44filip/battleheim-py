ec03b9d7ce844c7122c7f45d499a2275472b3b63
S-Filip <100999946+S-Filip@users.noreply.github.com>
2024-02-13 06:46:39 +0100

    Character class and attributes
    Setting up the Character class alongside maximum health and minimum health (when attacked)

---
+++ character.py
@@ -0,0 +1,11 @@
+class Character:
+    def __init__(self, name: str, health: int, damage: int):
+        self.name = name
+        self.health = health
+        self.health_max = health
+        self.damage = damage
+        
+    def attack(self, target) -> None:
+        target.health -= self.damage
+        target.health = max(target.hp, 0)
+        # Prevents health value going below zero
