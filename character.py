a67970a8d324a7dffe71d805ce9da1b478efd7f3
S-Filip <100999946+S-Filip@users.noreply.github.com>
2024-02-13 07:02:38 +0100

    Adding extra print info
    Added commentary with weapon type, damage and source included

--- character.py
+++ character.py
@@ -11,6 +11,7 @@ class Character:
         target.health -= self.weapon.damage
         target.health = max(target.health, 0)
         # Prevents health value going below zero
+        print(f"{self.name} dealt {self.weapon.damage} damage to {target.name} using (a) {self.weapon.name}")
         
 class Hero(Character):
     def __init__(self, name: str, health: int):
