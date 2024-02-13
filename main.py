d90684a706597ceac430b39b30ef5dad4c584275
S-Filip <100999946+S-Filip@users.noreply.github.com>
2024-02-13 07:00:28 +0100

    Fixing redundant argument
    Removing deprecated damage stat

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from character import Hero, Enemy
 
-hero = Hero(name = "Hero", health = 100, damage = 5)
-enemy = Enemy(name = "Enemy", health = 100, damage = 3)
+hero = Hero(name = "Hero", health = 100)
+enemy = Enemy(name = "Enemy", health = 100)
 
 while hero.health > 0 and enemy.health > 0:
     hero.attack(enemy)
