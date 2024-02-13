6c9a972953fb9f4b174a323143e3674c4c00d8fe
S-Filip <100999946+S-Filip@users.noreply.github.com>
2024-02-13 07:20:32 +0100

    Defining health bar values
    Setting needed values and defaults, alongside a colors dict

--- health_bar.py
+++ health_bar.py
@@ -1,1 +1,33 @@
 import os
+
+os.system("")
+# Hotfix for console not printing colors
+
+class HealthBar:
+    symbol_remaining: str = "█"
+    symbol_lost: str = "_"
+    barrier: str = "|"
+    colors: dict = {"red": "\033[91m",
+                    "purple": "\33[95m",
+                    "blue": "\33[34m",
+                    "blue2": "\33[36m",
+                    "blue3": "\33[96m",
+                    "green": "\033[92m",
+                    "green2": "\033[32m",
+                    "brown": "\33[33m",
+                    "yellow": "\33[93m",
+                    "grey": "\33[37m",
+                    "default": "\033[0m"
+                    }
+    
+    def __init__(self,
+                 entity,
+                 length: int = 20,
+                 is_colored: bool = True,
+                 color: str = "") -> None:
+        self.entity = entity
+        self.length = length
+        self.max_value = entity.health_max
+        self.current_value = entity.health
+        self.is_colored = is_colored
+        self.color = color
