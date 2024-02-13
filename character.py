614c831764659a6074ef5390bec7e94cd725309f
S-Filip <100999946+S-Filip@users.noreply.github.com>
2024-02-13 06:56:26 +0100

    Setting up a weapon class
    Assembling weapon class that represents character damage, removing self.damage stat and equipping each character with fists by default.

--- character.py
+++ character.py
@@ -1,11 +1,13 @@
+from weapon import fists
+
 class Character:
     def __init__(self, name: str, health: int, damage: int):
         self.name = name
         self.health = health
         self.health_max = health
-        self.damage = damage
+        self.weapon = fists
         
     def attack(self, target) -> None:
-        target.health -= self.damage
+        target.health -= self.weapon.damage
         target.health = max(target.health, 0)
         # Prevents health value going below zero
